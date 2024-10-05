a037c6a6e1f876a934e85cac8cf96910568f01c5
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2024-10-05 12:46:15 -0300

    Iniciando o projeto da tela de login

---
+++ src/pages/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row,} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Login = () => {
+  return (
+    <main>
+      <Container className="mt-5">
+        <Row>
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default Login;
